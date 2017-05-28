42320250fc3d9a12ffaccad38f0218d89f6faae2
mrcpp <gimrcpp@gmail.com>
2017-05-28 15:32:38 +0300

    updates

--- app/src/main/java/com/shotdrop/FragmentSettings.java
+++ app/src/main/java/com/shotdrop/FragmentSettings.java
@@ -1,33 +1,49 @@
 package com.shotdrop;
 
-import android.Manifest;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.SwitchPreference;
 import android.widget.ListView;
 
-public class FragmentSettings extends PreferenceFragment {
+import com.dropbox.core.android.Auth;
 
-    private SwitchPreference enableDropboxAccount;
-    private SwitchPreference enableApplication;
-    private SwitchPreference enableUploadOnlyByWifi;
-    private SwitchPreference enableStartAfterReboot;
+public class FragmentSettings extends PreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref_settings);
 
-        enableDropboxAccount = (SwitchPreference) getPreferenceManager()
-                .findPreference("enableDropboxAccount");
-        enableApplication = (SwitchPreference) getPreferenceManager()
-                .findPreference("enableApplication");
-        enableUploadOnlyByWifi = (SwitchPreference) getPreferenceManager()
-                .findPreference("enableUploadOnlyByWifi");
-        enableStartAfterReboot = (SwitchPreference) getPreferenceManager()
-                .findPreference("enableStartAfterReboot");
+        SwitchPreference enableDropboxAccount = (SwitchPreference) getPreferenceManager()
+                .findPreference(Prefs.ENABLE_DROPBOX_ACCOUNT);
+        enableDropboxAccount.setOnPreferenceChangeListener(this);
+        SwitchPreference enableApplication = (SwitchPreference) getPreferenceManager()
+                .findPreference(Prefs.ENABLE_APPLICATION);
+        enableApplication.setOnPreferenceChangeListener(this);
+        SwitchPreference enableUploadOnlyByWifi = (SwitchPreference) getPreferenceManager()
+                .findPreference(Prefs.ENABLE_UPLOAD_ONLY_BY_WIFI);
+        enableUploadOnlyByWifi.setOnPreferenceChangeListener(this);
+        SwitchPreference enableStartAfterReboot = (SwitchPreference) getPreferenceManager()
+                .findPreference(Prefs.ENABLE_START_AFTER_REBOOT);
+        enableStartAfterReboot.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        switch (preference.getKey()) {
+            case Prefs.ENABLE_DROPBOX_ACCOUNT:
+                Auth.startOAuth2Authentication(getActivity(), getString(R.string.app_key));
+                return false;
+            case Prefs.ENABLE_APPLICATION:
+                break;
+            case Prefs.ENABLE_UPLOAD_ONLY_BY_WIFI:
+                break;
+            case Prefs.ENABLE_START_AFTER_REBOOT:
+                break;
+        }
+        return true;
     }
 
     @Override
