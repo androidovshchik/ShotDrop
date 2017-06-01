25abd9763a84c9743d86cd29fb901e3618f4142d
mrcpp <gimrcpp@gmail.com>
2017-06-01 09:11:40 +0300

    updates

--- app/src/main/java/com/shotdrop/receivers/DeviceBootReceiver.java
+++ app/src/main/java/com/shotdrop/receivers/DeviceBootReceiver.java
@@ -4,14 +4,28 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.shotdrop.ServiceMain;
+import com.shotdrop.utils.Prefs;
+
 public class DeviceBootReceiver extends BroadcastReceiver {
 
     public static final String ACTION = Intent.ACTION_BOOT_COMPLETED;
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Prefs prefs = new Prefs(context);
         switch(intent.getAction()) {
             case ACTION:
+                boolean isServiceRunning = ServiceMain.isRunning(context);
+                if (prefs.getBoolean(Prefs.ENABLE_APPLICATION)) {
+                    if (!isServiceRunning) {
+                        context.startService(ServiceMain.getStartIntent(context));
+                    }
+                } else {
+                    if (isServiceRunning) {
+                        context.stopService(ServiceMain.getStartIntent(context));
+                    }
+                }
                 break;
             default:
                 break;
