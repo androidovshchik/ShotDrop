fef373da60c93a2bde400332a4f816cf15407e71
mrcpp <gimrcpp@gmail.com>
2017-06-04 10:19:42 +0300

    updates

--- app/src/main/java/com/shotdrop/observers/NotificationListenerClass.java
+++ app/src/main/java/com/shotdrop/observers/NotificationListenerClass.java
@@ -1,10 +1,13 @@
 package com.shotdrop.observers;
 
+import android.app.Notification;
 import android.content.Intent;
 import android.os.IBinder;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 
+import com.shotdrop.ServiceMain;
+import com.shotdrop.utils.LogUtil;
 import com.shotdrop.utils.Prefs;
 
 import java.io.File;
@@ -16,6 +19,8 @@ import java.util.List;
 import timber.log.Timber;
 
 public class NotificationListenerClass extends NotificationListenerService {
+
+    public static final String COM_ANDROID_SYSTEMUI = "com.android.systemui";
 
     private ScreenshotCallback callback;
 
@@ -30,9 +35,20 @@ public class NotificationListenerClass extends NotificationListenerService {
 
     @Override
     public void onNotificationPosted(StatusBarNotification notification) {
+        //if (!ServiceMain.isRunning(getApplicationContext())) {
+        //    return;
+        //}
+        if (prefs == null) {
+            prefs = new Prefs(getApplicationContext());
+        }
+        logNotification(notification);
         switch (notification.getPackageName()) {
+            case COM_ANDROID_SYSTEMUI:
+                if (notification.getNotification().contentIntent != null) {
+                    Timber.d("onScreenshotReady");
+                }
+                break;
             default:
-                Timber.d(notification.getPackageName());
                 break;
         }
     }
@@ -71,5 +87,26 @@ public class NotificationListenerClass extends NotificationListenerService {
             Timber.e(e.getLocalizedMessage());
             return 0;
         }
+    }
+
+    private void logNotification(StatusBarNotification notification) {
+        String classname = getClass().getSimpleName();
+        LogUtil.logDivider(classname, ":");
+        LogUtil.logCentered(" ", classname, "New notification");
+        LogUtil.logCentered(":", classname, "packageName: " + notification.getPackageName());
+        LogUtil.logCentered(":", classname, "id: " + notification.getId());
+        if (notification.getNotification().actions != null) {
+            LogUtil.logCentered(" ", classname, "Notification actions");
+            for (Notification.Action action : notification.getNotification().actions) {
+                LogUtil.logCentered(":", classname, "action.title: " + action.title);
+            }
+        }
+        if (notification.getNotification().extras != null) {
+            LogUtil.logCentered(" ", classname, "Notification extras");
+            for (String key : notification.getNotification().extras.keySet()) {
+                Timber.d(key + ": " + notification.getNotification().extras.get(key));
+            }
+        }
+        LogUtil.logDivider(classname, ":");
     }
 }
